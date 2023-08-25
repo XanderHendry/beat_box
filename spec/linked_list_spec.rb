90239ea0a63d2f690d734209880e6c96517d999c
Xander Hendry <vhendry.coding@gmail.com>
2023-08-24 20:21:15 -0400

    Add more tests to linked_list_spec

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -12,4 +12,21 @@ RSpec.describe Linked_list do
       expect(list.head).to eq(nil)
     end
 
+    it 'has a head that does not have to be nil'
+      list = Linked_list.new("Head")
+      expect(list.head).to eq("Head")
+    end
+
+    it 'has a head whose value can be changed'
+      list = Linked_list.new
+      expect(list.head).to eq(nil)
+      list.head = "Head"
+      expect(list.head).to eq("Head")
+    end
+
+    xit 'has a head that can be a Node'
+      node = Node.new
+      list = Linked_list.new(node)
+      expect(list.head).to be_instance_of(Node)
+    end
 end
